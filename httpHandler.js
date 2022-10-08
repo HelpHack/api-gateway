72f72b31907ac6791fd0e045b96ca65e79021032
Adam Oleszko <adamoleszko44@gmail.com>
2022-10-08 16:20:44 +0200

    Add http handler

---
+++ httpHandler.js
@@ -0,0 +1,91 @@
+const DEFAULT_TIMEOUT = process.env.TEST ? 1000 : 30000;
+const TIMEOUT = parseInt(process.env.REQUEST_TIMEOUT || DEFAULT_TIMEOUT);
+
+class HttpHandler {
+  constructor(queueManager) {
+    this.queueManager = queueManager;
+
+    this.handleRequest = this.handleRequest.bind(this);
+  }
+
+  static getQueueName(path) {
+    console.log({ path });
+    const parts = path.split('/');
+
+
+    return parts[1]
+  }
+
+  async _toRabbitmqRequest(req) {
+    const { headers } = req;
+    const body = await HttpHandler._readBody(req);
+
+    const rabbitRequest = {
+      data: body,
+      headers,
+      path: req.originalUrl,
+      timestamp: Date.now(),
+      createdAt: new Date().toISOString(),
+      method: req.method,
+    };
+
+    return rabbitRequest;
+  }
+
+  static _readBody(req) {
+    return new Promise((resolve, reject) => {
+      let body = [],
+        hasBody = false;
+      req
+        .on('data', chunk => {
+          hasBody = true;
+          body.push(chunk);
+        })
+        .on('end', () => {
+          if (hasBody) {
+            body = Buffer.concat(body).toString();
+            resolve(body);
+          } else {
+            resolve(null);
+          }
+        })
+        .on('error', reject);
+    });
+  }
+
+  async handleRequest(req, res, next) {
+    console.log({ req });
+    const startTime = new Date().getTime();
+    const queueName = HttpHandler.getQueueName(req.path);
+    res.send(queueName)
+    const start = new Date().getTime();
+
+    let statusCode = 0;
+
+    try {
+      const rabbitRequest = await this._toRabbitmqRequest(req);
+
+      const result = await this.queueManager.query(
+        queueName,
+        rabbitRequest,
+        { mandatory: true, immediate: true },
+        TIMEOUT,
+      );
+
+      const { headers } = result;
+      statusCode = result.statusCode;
+      const data = result.data
+
+      if (headers) {
+        res.set(headers);
+      }
+      if (rabbitRequest && result)
+      res.status(statusCode).send(data);
+    } catch (err) {
+      console.log(err)
+      res.status(500).send({ error: 'server error' });
+    }
+  }
+}
+
+export default HttpHandler;
---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import createRabbitManager from './rabbit.js';
+import {msgbus, Router} from 'helphack-router'
+import HttpHandler from "./httpHandler.js";
+
+const app = express();
+const port = process.env.PORT || 3000
+let queueManager, router, httpHandler
+const init = async () => {
+  queueManager = await createRabbitManager();
+  router = new Router(queueManager);
+  httpHandler = new HttpHandler(queueManager)
+}
+init()
+
+app.get('/*', async (req, res, next) => {
+
+  await httpHandler.handleRequest(req,res, next)
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
