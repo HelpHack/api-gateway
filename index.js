1b544db64213455b9ac47cc8c3c88accd1951a34
KrosFire <hubik080@gmail.com>
2022-10-09 04:25:47 +0200

    Added CRUD for requests

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ import { DirectionsController } from "./directions/Directions/DirectionsControll
 import { DirectionsService } from "./directions/Directions/DirectionsService.js";
 import { MapApi } from "./directions/MapApi/MapApi.js";
 import { BestTimeApi } from "./directions/BestTimeApi/BestTimeApi.js";
+import Database from './database/index.js';
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000
 const bestTimeApi = new BestTimeApi()
 const mapApi = new MapApi()
 const directionsService = new DirectionsService(mapApi, bestTimeApi)
 const directionsController = new DirectionsController(app, directionsService)
+const database = new Database(app).start()
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
